f6931c502ca00e702a98e7c1880aba1bc4d9d6b0
null096 <33376471+null096@users.noreply.github.com>
2018-06-06 22:14:13 +0300

    Fix for '/start' message + added greeting

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const minPicSize		= 350;
 const safePicSizeLimit	= 1500;
 
 bot.onText(/.+/, async function(msg, match) {
+	if (msg.message_id == 1) return;
 	var response	= match[0];
 	var userId		= msg.from.id;
 	var chatId		= msg.chat.id;
@@ -13,4 +14,11 @@ bot.onText(/.+/, async function(msg, match) {
 	var picUrl		= `http://api.qrserver.com/v1/create-qr-code/?data=${ response }&size=${ picSize }x${ picSize }&qzone=1`;
 	await bot.sendMessage(userId, `Wait... ${ response.length > safePicSizeLimit ? 'Warning: Large text, may not work' : ''}`);
 	bot.sendPhoto(chatId, picUrl);
+});
+
+bot.onText(/^\/start$/, function(msg, match) {
+	if (msg.message_id != 1) return;
+	var userId 		= msg.from.id;
+	var userName	= msg.from.username;
+	bot.sendMessage(userId, `Hello, ${ userName }, send me the text, and I convert it to QR-code!`);
 });
