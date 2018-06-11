349d221a108b76c75c075e235869ad9299bd09c0
null096 <33376471+null096@users.noreply.github.com>
2018-06-11 15:18:33 +0300

    Refactoring

--- bot.js
+++ bot.js
@@ -1,55 +1,52 @@
-const request			= require('request');
-const TelegramBot		= require('node-telegram-bot-api');
-const token				= process.env.TELEGRAM_TOKEN;
-const bot				= new TelegramBot(token, { polling: true });
-const minPicSize		= 350;
-const safePicSizeLimit	= 1200;
-const maxPicSizeLimit	= 3000;
+const request					= require('request');
+const TelegramBot				= require('node-telegram-bot-api');
+const token						= process.env.TELEGRAM_TOKEN;
+const bot						= new TelegramBot(token, { polling: true });
+const minImageSize				= 350;
+const safeTextSizeForEncoding	= 1200;
+const maxTextSizeForEncoding	= 3000;
 
 
-bot.on("message", async function(msg, match) {
-	var userId		= msg.from.id;
-	var chatId		= msg.chat.id;
-	var response	= msg.text;
-	var photos		= msg.photo;
-
+bot.on("message", async function(msg) {
 	if ( msg.message_id == 1 ) return;
-	if ( !response && !photos ) {
+
+	var userId	= msg.from.id;
+	var chatId	= msg.chat.id;
+	var text	= msg.text;
+	var photos	= msg.photo;
+
+	if ( !(text || photos) ) {
 		bot.sendMessage(userId, 'Wrong data');
 		return;
 	}
 
-	if ( response ) {
-		encodeText(userId, chatId, response);
+	if ( text ) {
+		encodeText(userId, chatId, text);
 	} else if ( photos ) {
-		decodePhoto(userId, chatId, photos[photos.length - 1]);
+		decodePhoto(userId, photos[photos.length - 1]);
 	}
 });
 
 
-bot.onText(/^\/start$/, function(msg, match) {
+bot.onText(/^\/start$/, function(msg) {
 	if ( msg.message_id != 1 ) return;
 	var userId 		= msg.from.id;
 	var userName	= msg.from.username;
-	bot.sendMessage(userId, 
-		`Hello, ${ userName }\nSend me the text, and I will convert it to QR-code!\nSend me the QR-code and I will convert it into text!`);
+	bot.sendMessage(userId, `Hello, ${ userName }\nSend me the text, and I will convert it to QR-code!\nSend me the QR-code and I will convert it into text!`);
 });
 
 
-async function encodeText(userId, chatId, response) {
-	var picSize		= minPicSize + (~~(response.length / 300) * 50);
-	var picURL		= `http://api.qrserver.com/v1/create-qr-code/?data=${ encodeURIComponent(response) }&size=${ picSize }x${ picSize }&qzone=1`;
-
-	if ( response.length > maxPicSizeLimit ) {
-		bot.sendMessage(userId, `Very large text, should be less than ${ maxPicSizeLimit } characters`);
+async function encodeText(userId, chatId, text) {
+	if ( text.length > maxTextSizeForEncoding ) {
+		bot.sendMessage(userId, `Very large text, should be less than ${ maxTextSizeForEncoding } characters`);
 		return;
 	}
-	if ( response.length > safePicSizeLimit ) {
+	if ( text.length > safeTextSizeForEncoding ) {
 		await bot.sendMessage(userId, `Warning: Large text, may not work`);
 	}
 
 	try {
-		await bot.sendPhoto(chatId, picURL);
+		bot.sendPhoto(chatId, getQrImageUrl(text));
 	}
 	catch(e) {
 		bot.sendMessage(userId, `Something went wrong`);
@@ -57,11 +54,10 @@ async function encodeText(userId, chatId, response) {
 }
 
 
-async function decodePhoto(userId, chatId, photo) {
-	var fileURL = await bot.getFileLink(photo.file_id);
-	var URL		= `http://api.qrserver.com/v1/read-qr-code/?fileurl=${ fileURL }`;
+async function decodePhoto(userId, photo) {
 	try {
-		var JSONresponse = (await getJSONFrom(URL))[0].symbol[0];
+		var textUrl			= await getTextUrl(photo);
+		var JSONresponse	= (await getJsonFrom(textUrl))[0].symbol[0];
 		bot.sendMessage(userId, JSONresponse.error || JSONresponse.data);
 	}
 	catch(e) {
@@ -70,9 +66,21 @@ async function decodePhoto(userId, chatId, photo) {
 }
 
 
-function getJSONFrom(URL) {
+function getQrImageUrl(text) {
+	var imageSize = minImageSize + (~~(text.length / 300) * 50);
+	return `http://api.qrserver.com/v1/create-qr-code/?data=${ encodeURIComponent(text) }&size=${ imageSize }x${ imageSize }&qzone=1`;
+}
+
+
+async function getTextUrl(photo) {
+	var photoUrl = await bot.getFileLink(photo.file_id);
+	return `http://api.qrserver.com/v1/read-qr-code/?fileurl=${ photoUrl }`;
+}
+
+
+function getJsonFrom(url) {
 	return new Promise((resolve, reject) =>
-		request(URL, function(error, response, body) {
+		request(url, function(error, response, body) {
 			if ( !error && response.statusCode == 200 ) {
 				resolve(JSON.parse(body));
 			} else {
